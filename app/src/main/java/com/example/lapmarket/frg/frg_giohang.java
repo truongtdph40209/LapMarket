cef0f7eaa8d9a0725f059cbf2478c333e948a7e1
Truong <truongtdph40209@fpt.edu.vn>
2023-11-22 20:17:22 +0700

    22/11 truong

--- app/src/main/java/com/example/lapmarket/frg/frg_giohang.java
+++ app/src/main/java/com/example/lapmarket/frg/frg_giohang.java
@@ -1,30 +1,21 @@
 package com.example.lapmarket.frg;
 
-import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.lapmarket.R;
 import com.example.lapmarket.adapter.GioHangAdapter;
 import com.example.lapmarket.dao.GioHangDAO;
 import com.example.lapmarket.inteface.TotalUpdateListener;
-import com.example.lapmarket.man_hinh_thanh_toan;
 import com.example.lapmarket.model.giohang;
-import com.example.lapmarket.model.hoadon;
 import com.example.lapmarket.util.Amount;
 
 import java.util.ArrayList;
